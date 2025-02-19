3e05e9cac83a474cf574b2b03cd41bd1ea3a1054
james13542 <102084468+james13542@users.noreply.github.com>
2025-02-18 20:40:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+import htmlContent from "./content.html";
+
 addEventListener("fetch", event => {
   event.respondWith(
-    new Response(content.html, {
-      headers: { "content-type": "text/html" },
+    new Response(htmlContent, {
+      headers: { "Content-Type": "text/html" }
     })
   );
 });
+
