4e5a6f05ab464ea910108b30d6db487f75723d40
james13542 <102084468+james13542@users.noreply.github.com>
2026-01-09 20:37:34 +0000

    index changed

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import htmlContent from "./content.html";
+import cssContent from "./style.css";
 
 addEventListener("fetch", event => {
+  const html = htmlContent.replace("</head>", `<style>${cssContent}</style></head>`);
   event.respondWith(
-    new Response(htmlContent, {
+    new Response(html, {
       headers: { "Content-Type": "text/html" }
     })
   );
